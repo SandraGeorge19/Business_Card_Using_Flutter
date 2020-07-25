54065c7405913118ec606943a5d19653b32fbfd4
Sandra George <georgesandra592@gmail.com>
2020-07-25 02:04:22 +0200

    All the Project commit

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('Images/Sandra.jpg'),
+              ),
+              Text(
+                'Sandra El Qomos',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.teal.shade100,
+                  fontFamily: 'SourceSansPro',
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                  thickness: 1.2,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+20  102  419  6555',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.teal,
+                      fontFamily: 'SourceSansPro',
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'sandrael_quiss@yahoo.com',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.teal,
+                      fontFamily: 'SourceSansPro',
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
